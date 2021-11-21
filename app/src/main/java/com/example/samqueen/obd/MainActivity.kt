93600c1509d8e8f6b5674e4e6ced835cad3c3bd2
Sam Queen <samqueenbusiness@gmail.com>
2021-11-21 01:44:13 -0700

    added bluetooth enable feature

--- app/src/main/java/com/example/samqueen/obd/MainActivity.kt
+++ app/src/main/java/com/example/samqueen/obd/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.example.samqueen.obd
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
+
+    var m_bluetoothadapter: BluetoothAdapter? = null;
+    lateinit var m_pairedDevices: Set<BluetoothDevice>;
+    val REQUEST_ENABLE_BLUETOOTH = 1;
+
+    companion object {
+        val EXTRA_ADDRESS: String = "Device_address"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("OBD2 Scanner App Starting")
+
+        m_bluetoothadapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(m_bluetoothadapter == null) {
+            toast("This device does not support bluetooth");
+            return
+        }
+
+        if(!m_bluetoothadapter!!.isEnabled) {
+            val enableBluethoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluethoothIntent, REQUEST_ENABLE_BLUETOOTH);
+        }
     }
+
+    private fun pairedDeviceList() {
+
+    }
+
+
 }
