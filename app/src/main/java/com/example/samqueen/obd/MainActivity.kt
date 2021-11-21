3de37426e288890bda44275f36a88f960480e22b
Sam Queen <samqueenbusiness@gmail.com>
2021-11-21 00:37:26 -0700

    First commit

---
+++ app/src/main/java/com/example/samqueen/obd/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.samqueen.obd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("OBD2 Scanner App Starting")
+    }
+}
