581b3e23ecf9d552f32b22c43ed03b4d6db64e13
Sam Queen <samqueenbusiness@gmail.com>
2021-11-21 03:41:09 -0700

    bluetooth refresh

---
+++ app/src/main/java/com/example/samqueen/obd/ControlActivity.kt
@@ -0,0 +1,10 @@
+package com.example.samqueen.obd
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ControlActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/samqueen/obd/MainActivity.kt
+++ app/src/main/java/com/example/samqueen/obd/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.samqueen.obd
 
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.example.samqueen.obd.ControlActivity
+import com.example.samqueen.obd.R
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
-    var m_bluetoothadapter: BluetoothAdapter? = null;
-    lateinit var m_pairedDevices: Set<BluetoothDevice>;
-    val REQUEST_ENABLE_BLUETOOTH = 1;
+    private var m_bluetoothAdapter: BluetoothAdapter? = null
+    private lateinit var m_pairedDevices: Set<BluetoothDevice>
+    private val REQUEST_ENABLE_BLUETOOTH = 1
 
     companion object {
         val EXTRA_ADDRESS: String = "Device_address"
@@ -21,22 +28,58 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        m_bluetoothadapter = BluetoothAdapter.getDefaultAdapter();
-
-        if(m_bluetoothadapter == null) {
-            toast("This device does not support bluetooth");
+        m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (m_bluetoothAdapter == null) {
+            toast("this device doesn't support bluetooth")
             return
         }
-
-        if(!m_bluetoothadapter!!.isEnabled) {
-            val enableBluethoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBluethoothIntent, REQUEST_ENABLE_BLUETOOTH);
+        if (!m_bluetoothAdapter!!.isEnabled) {
+            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
         }
+
+        select_device_refresh.setOnClickListener { pairedDeviceList() }
+
     }
 
     private fun pairedDeviceList() {
+        m_pairedDevices = m_bluetoothAdapter!!.bondedDevices
+        val list: ArrayList<BluetoothDevice> = ArrayList()
 
+        if (!m_pairedDevices.isEmpty()) {
+            for (device: BluetoothDevice in m_pairedDevices) {
+                list.add(device)
+                Log.i("device", "" + device)
+            }
+        } else {
+            toast("no paired bluetooth devices found")
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        select_device_list.adapter = adapter
+        select_device_list.onItemClickListener =
+            AdapterView.OnItemClickListener { _, _, position, _ ->
+                val device: BluetoothDevice = list[position]
+                val address: String = device.address
+
+                val intent = Intent(this, ControlActivity::class.java)
+                intent.putExtra(EXTRA_ADDRESS, address)
+                startActivity(intent)
+            }
     }
 
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_ENABLE_BLUETOOTH) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (m_bluetoothAdapter!!.isEnabled) {
+                    toast("Bluetooth has been enabled")
+                } else {
+                    toast("Bluetooth has been disabled")
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                toast("Bluetooth enabling has been canceled")
+            }
+        }
+    }
 }
